406523550c0b3abeeec393a967be91ef6f5ccd72
Your name <lowejosh50@gmail.com>
2018-06-11 00:24:34 +1000

    more progress with setup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Global variables
 let w, h, c, ctx;
+let startedTyping = false;
 
 // Entry point
 function init() {
@@ -8,14 +9,10 @@ function init() {
     c.width = w = window.innerWidth;
     c.height = h = window.innerHeight;
 
-
-
     // Set the context
     ctx = c.getContext("2d");
-
     if( c.getContext ) {
         setup();
-//        setInterval( run , 33 );
     }
 }
 
@@ -25,8 +22,8 @@ function init() {
 function setup() {
     // Clear the screen and create the start page
     clearScreen();
-    centerText("TypeFall", -20, 36);
-    centerText("Press any key to begin", 20, 24);
+    centerText(w / 2, h / 2, "TypeFall", -20, 36, "normal", "#59C9A5");
+    centerText(w / 2, h / 2, "Press any key to begin", 20, 24, "normal", "#59C9A5");
     // Wait for any key press
     window.addEventListener("keypress", startGame);
 }
@@ -38,11 +35,13 @@ function startGame(e) {
 }
 
 function process() {
-   drawBorder();
+    clearScreen();
+    drawBorder();
 }
 
 function drawBorder() {
     const THICKNESS = 36;
+    const BRACE = w / 4;
 
     ctx.fillStyle = "#59C9A5";
     ctx.fillRect(0, h - THICKNESS, w, THICKNESS);
@@ -50,23 +49,31 @@ function drawBorder() {
     ctx.fillRect(0, h - 2 * THICKNESS, w, THICKNESS);
 
     ctx.fillStyle = "#3B2C35";
-    ctx.fillRect(0, h - 2 * THICKNESS, 300, THICKNESS);
-    ctx.fillRect(w - 300, h - 2 * THICKNESS, 300, THICKNESS);
+    ctx.fillRect(0, h - 2 * THICKNESS, BRACE, THICKNESS);
+    ctx.fillRect(w - BRACE, h - 2 * THICKNESS, BRACE, THICKNESS);
 
+    if (!startedTyping) {
+        ctx.fontStyle = "italic";
+        centerText(w / 2, h - 2 * THICKNESS + (THICKNESS / 2), "start typing", 0, 18, "italic", "#5B6C5D");
+    }
 }
 
 /**
- * Centers text in the canvas with specified font size and offset
+ * Centers text at x/y with specified font size and offset
+ * @param x x position
+ * @param y y position
  * @param text text to center
  * @param yOffset offset value added to the y axis
  * @param fontSize size of the font
+ * @param fontStyle style of the font
+ * @param color color of the text
  */
-function centerText(text, yOffset, fontSize) {
-        ctx.fillStyle = "#56E39F";
-        ctx.font = fontSize + "px Arial";
+function centerText(x, y, text, yOffset, fontSize, fontStyle, color) {
+        ctx.fillStyle = color;
+        ctx.font = fontStyle + " " + fontSize + "px Arial";
         ctx.textBaseline = "middle";
         ctx.textAlign = "center"
-        ctx.fillText(text, w / 2, (h / 2) + yOffset);
+        ctx.fillText(text, x, y + yOffset);
         test = true;
 }
 
